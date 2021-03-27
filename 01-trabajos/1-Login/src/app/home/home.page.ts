bca09e013623bd0124a6e7bb7cff8757e22cfcf9
agustinrv <agustinrivola2000@gmail.com>
2021-03-27 14:24:25 -0300

    splash

---
+++ 01-trabajos/1-Login/src/app/home/home.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import {User} from '../shared/User.class';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+    user:User = new User();
+    flag:boolean =false;
+
+
+  constructor(private router:Router , private authSvc : AuthService) {}
+
+  ngOnInit(){}
+
+  async onLogin()
+  {
+    const user= await this.authSvc.onLogin(this.user);
+
+    if(user)
+    {
+      console.log('Successfully logged in!!');
+      alert("BIENVENIDOOOOO");
+      this.router.navigateByUrl("/principal");
+      this.flag=false;
+    }
+    else
+    {
+      this.flag=true;
+    }
+
+
+  }
+
+}
